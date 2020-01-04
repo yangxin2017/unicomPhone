ea9b1f89c0cffce895c278c9190ccbbe4d5f7ae9
Jet Geng <jetgeng@gmail.com>
2020-01-04 13:36:09 +0800

    remove pipe

---
+++ src/pages/equip/device-type.pip.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({name:'deviceType'})
+export class DeviceTypeInfoPipe implements PipeTransform {
+  transform(value: string): string {
+    switch(value){
+        case '0': return '其他';
+        case '1': return '手机';
+        case '2': return '平板';
+        case '3': return '电脑';
+        default: return '未知';
+    } 
+  }
+}
